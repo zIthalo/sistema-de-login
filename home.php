3385300af695d39d4cb4d1290c7e1ffcbbb9c5cf
zIthalo <ithalowillian@gmail.com>
2023-05-30 00:19:24 -0300

    updates de arquivos

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>zGladio</title>
+    <link rel="stylesheet" href="./style/home.css">
+</head>
+
+<body>
+    <h1>Tudo que você sempre quis saber em um único lugar</h1>
+    <div class="box">
+        <a href="login.php">Login</a>
+        <a href="formulario.php">Cadastre-se</a>
+    </div>
+</body>
+
+</html>
