7b6d3270c1c718ec8adf14f72c2a409c14b0aea1
zIthalo <ithalowillian@gmail.com>
2023-06-02 22:50:31 -0300

    atualizando

--- testLogin.php
+++ testLogin.php
@@ -1,6 +1,11 @@
 <?php
+//Neste código aqui vamos fazer os testes se a pessoa está cadastrada ou não
+
     session_start();
+
     // print_r($_REQUEST);
+    
+    //Se o meu submit existir e meu campo email não estiver vazio e minha senha não estiver vazia então acesse
     if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']))
     {
         // Acessa
@@ -19,22 +24,21 @@
         // print_r($sql);
         // print_r($result);
 
-        if(mysqli_num_rows($result) < 1)
-        {
-            unset($_SESSION['email']);
-            unset($_SESSION['senha']);
-            header('Location: login.php');
+        if(mysqli_num_rows($result) < 1){ //Se o resultado que o usuario digitou de email e senha for menor que uma linha lá no meu banco de dados, destrua o email que ele digitou e a senha que ele digitou.
+            
+            unset($_SESSION['email']);//destroi o email
+            unset($_SESSION['senha']);//destroi a senha
+            header('Location: login.php');//volta para a tela de login
         }
-        else
-        {
-            $_SESSION['email'] = $email;
-            $_SESSION['senha'] = $senha;
-            header('Location: sistema.php');
+        else{//se não
+            $_SESSION['email'] = $email; //cria uma nova sessão com o emal
+            $_SESSION['senha'] = $senha; //cria uma nova sessão com a senha
+            header('Location: sistema.php'); //manda para a tela do sistema.php
         }
     }
     else
     {
-        // Não acessa
-        header('Location: login.php');
+        // isso aqui faz eu retornar para a tela de login caso eu não esteja logado ou cadastrado no DB
+        header('Location: login.php');// é essa a sintaxe que usamos para direcionar alguém para alguma tela.
     }
 ?>
